b88dc41a74bb6784ba786553ade4b354e38c12e5
CsokiVok <kalinatamas20@gmail.com>
2024-05-07 16:09:29 +0200

    Játék vége

--- madar/Assets/scrip.cs
+++ madar/Assets/scrip.cs
@@ -7,20 +7,32 @@ public class scrip : MonoBehaviour
 
     public Rigidbody2D myRigidbody;
     public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) == true)
+        if(Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
             myRigidbody.velocity = Vector2.up * flapStrength;
         }
+        if(transform.position.y > 6 || transform.position.y < -6)
+        {
+            logic.gameOver();
+        }
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive  = false;
     }
 }
